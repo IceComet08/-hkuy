cb77bc011402f49bfa107c8386860804fdaf8773
IceComet08 <93005347+IceComet08@users.noreply.github.com>
2023-07-21 14:33:38 -0300

    Add files via upload

---
+++ projeto  103/file_system_events_tracker.py
@@ -0,0 +1,35 @@
+import os
+import sys
+import time
+import random
+import shutil
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+from_dir = "C:/Users/ferfu/Downloads"
+
+class FileEventHandler(FileSystemEventHandler):
+
+    def on_created(self, event):
+        print(f"Olá, {event.src_path} foi criado!")
+
+    def on_deleted(self, event):
+        print(f"Opa! Alguém excluiu {event.src_path}!")
+
+    def on_modified(self, event):
+        print(f"Opa!, alguém modificou {event.src_path} ")
+    
+    def on_moved(self, event):
+        print(f"Alguém moveu {event.src_path} para {event.dest_path}")
+
+event_handler = FileEventHandler()
+observer = Observer()
+observer.schedule(event_handler, from_dir, recursive = True)
+observer.start()
+try:
+    while True:
+        time.sleep(2)
+        print("executando")
+except KeyboardInterrupt:
+    print("interrompido")
+    observer.stop()
